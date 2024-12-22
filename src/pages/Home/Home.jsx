374b56639a75e6bc012a1ec2da60ecdde628635d
luuhuy2402 <xeoxeo1895@gmail.com>
2024-12-22 21:26:57 +0700

    Home - section3 b: báBá»urgers list

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Layout from "../../components/Layouts/Layout";
 import Section1 from "./Section1";
 import "../../styles/HomeStyle.css";
 import Section2 from "./Section2";
+import Section3 from "./Section3";
 export default function Home() {
     return (
         <>
@@ -11,6 +12,9 @@ export default function Home() {
 
                 {/* Home Section About */}
                 <Section2 />
+
+                {/* Home Section Menu */}
+                <Section3 />
             </Layout>
         </>
     );
