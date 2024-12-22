1a13c85ce6bcd57df2b552e67d6ff5f89038988e
luuhuy2402 <xeoxeo1895@gmail.com>
2024-12-22 20:16:58 +0700

    Home - section2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import Layout from "../../components/Layouts/Layout";
 import Section1 from "./Section1";
-import "../../styles/HomeStyle.css"
+import "../../styles/HomeStyle.css";
+import Section2 from "./Section2";
 export default function Home() {
     return (
         <>
             <Layout>
                 {/* Home section hero banner */}
                 <Section1 />
+
+                {/* Home Section About */}
+                <Section2 />
             </Layout>
         </>
     );
