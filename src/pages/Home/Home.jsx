a36c0af49519139ab28b307ca8e60667495a0f37
luuhuy2402 <xeoxeo1895@gmail.com>
2024-12-28 05:09:52 +0700

    Home section6 Blog

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import Section2 from "./Section2";
 import Section3 from "./Section3";
 import Section4 from "./Section4";
 import Section5 from "./Section5";
+import Section6 from "./Section6";
 export default function Home() {
     return (
         <>
@@ -23,6 +24,9 @@ export default function Home() {
 
                 {/* Home Section Shop */}
                 <Section5 />
+
+                {/* Home Section Blog */}
+                <Section6 />
             </Layout>
         </>
     );
